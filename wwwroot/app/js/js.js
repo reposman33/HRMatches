b64a9ecfa1e0d31d5c2d592204572f8b4bdaa4e3
Marc <info@marcbakker.com>
2016-02-15 22:54:53 +0100

    2e commit...

--- wwwroot/app/js/js.js
+++ wwwroot/app/js/js.js
@@ -3,15 +3,24 @@ angular.module('ontdekjouwtalent',[])
 .controller('loginController', function($scope,$http){
 	$scope.loginSubmit = function(){
 		// zet de headers voor POST requests als FOR post ipv standaard de 'application/json'
-		$http.defaults.headers.post = {'Content-type':'application/x-www-form-urlencoded; charset=utf-8'}
+		//$http.defaults.headers.post = {'Content-type':'application/x-www-form-urlencoded; charset=utf-8'}
 
 		$http({
-			method: 'POST',
-			url: 'http://ontdekjouwtalent.local/login.cfm',
-			data: 'loginName=' + $scope.loginName + '&loginPassword=' + $scope.loginPassword
+			method: 'GET',
+			url: 'http://api-development.hrmatches.com/authenticate',
+			data: {username: $scope.loginName, password:$scope.loginPassword}
 		})
 		.then(
-		    function(response){/*on success handler*/ console.log(response)},
+			function(response){
+				/*on success handler*/
+				console.log(response);
+				/*doe de standaard login*/
+				$http({
+					method:'POST',
+					url: 'index.cfm?module=security&view=login&action=login',
+					data:'j_username=' + $scope.loginName + '&j_password=' + $scope.loginPassword
+				})
+			},
 			function(response){/* on error handler*/ console.log(response)}
 		)
 	}
@@ -20,14 +29,19 @@ angular.module('ontdekjouwtalent',[])
 			method: 'POST',
 			url: '',
 			data: 'loginName=' + $scope.loginName
-		});
+		}).then(
+			function(response){/*on success handler*/ console.log(response)},
+			function(response){/* on error handler*/ console.log(response)}
+		);
 	}
 	$scope.register = function(){
 		$http({
 			method: 'POST',
 			url: '',
 			data: 'loginName=' + $scope.loginName + '&loginPassword=' + $scope.loginPassword
-
-		});
+		}).then(
+			function(response){/*on success handler*/ console.log(response)},
+			function(response){/* on error handler*/ console.log(response)}
+		);
 	}
 });
