5183c9733756020d91cf5a9f79a148ccd17258fd
Marc Bakker <info@marcbakker.com>
2016-05-17 14:33:21 +0200

    Settings menu: een item verwijderen uit de settingsMenuData.json lijst moet niet uitmaken voor de overige items - toon juiste stijl bij elke button.

--- wwwroot/app/components/settings/SettingsController.js
+++ wwwroot/app/components/settings/SettingsController.js
@@ -9,6 +9,15 @@ angular.module('app.ontdekJouwTalent')
 .controller('SettingsController',
 	['$scope','menu',
     function($scope,menu) {
+		$scope.styles = {
+			'/settings/myaccount': 'fa fa-user fa-fw',
+			'/settings/mycompany': 'fa fa-building-o fa-fw',
+			'/settings/userManagement': 'fa fa-users fa-fw',
+			'/settings/tagmanagement': 'fa fa-tags fa-fw',
+			'/settings/doctemplates': 'fa fa-files-o fa-fw',
+			'/settings/references': 'fa fa-th fa-fw',
+			'/settings/matching': 'fa fa-tasks  fa-fw'
+		}
 
         $scope.menu = menu;
 
