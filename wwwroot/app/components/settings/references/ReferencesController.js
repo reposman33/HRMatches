3276b330b7ccec24cded1e12e5b172fbf0761100
Marc Bakker <info@marcbakker.com>
2016-05-30 10:09:46 +0200

    Reload references na Save of Delete actie.

--- wwwroot/app/components/settings/references/ReferencesController.js
+++ wwwroot/app/components/settings/references/ReferencesController.js
@@ -6,18 +6,29 @@
  * Referring states:'settings.references
  * */
 angular.module('app.ontdekJouwTalent')
-.controller('ReferencesController',['$scope','AppConfig','APIService','data',function($scope,AppConfig,APIService,data){
+.controller('ReferencesController',['$scope','$state','AppConfig','APIService','data',function($scope,$state,AppConfig,APIService,data){
 
 	$scope.data = data;
 	$scope.showEditRow = false;
 
 	$scope.delete = function(id){
-		APIService.call({endpoint:AppConfig.API_ENDPOINTS.settings.references.endpoint,method:'DELETE'},{id:id});
+		APIService.call({endpoint:AppConfig.API_ENDPOINTS.settings.references.endpoint,method:'DELETE'},{id:id})
+		.then(
+			function(successResponse){
+				$state.reload($state.current);
+			}
+		)
+
 	}
 
 	$scope.save = function(reference){
 		var method = (reference.id != undefined && reference.id.length > 0) ? 'PUT' : 'POST';
-		APIService.call({endpoint:AppConfig.API_ENDPOINTS.settings.references.endpoint,method:method},{reference:reference});
+		APIService.call({endpoint:AppConfig.API_ENDPOINTS.settings.references.endpoint,method:method},{reference:reference})
+		.then(
+			function(successResponse){
+				$state.reload($state.current);
+			}
+		)
 	}
 	
 }])
